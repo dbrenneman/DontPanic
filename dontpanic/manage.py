a3a16608712a84d5abcf7224daed5b5f71505b78
David Brenneman <db@davidbrenneman.com>
2012-12-15 16:01:31 +0000

    added management script

---
+++ dontpanic/manage.py
@@ -0,0 +1,14 @@
+from flask.ext.script import Manager, Server
+
+from . import app
+
+manager = Manager(app)
+
+manager.add_command("run", Server(
+    use_debugger=True,
+    use_reloader=True,
+    host='0.0.0.0')
+)
+
+if __name__ == "__main__":
+    manager.run()
